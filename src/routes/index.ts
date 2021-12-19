25b316e11964f3005e6f46479fa6aef8caa63f07
blvkroot <hsalukamba17@gmail.com>
2021-12-19 01:58:15 +0100

    default endpoint / route created with project description

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,16 @@ import { encriptRouter } from './encripts/encript.routes';
 
 const router = Router();
 
+// Home default route describing project
+router.get('/', (request, response) => {
+    response.json({
+        "version": "1.0.0",
+        "author": "Henriques Salucamba",
+        "project-name": "Facile-Challenge",
+        "description": "This project aims at encrypting and decrypting a given string"
+    });
+});
+
 router.use(encriptRouter);
 
 export { router }
