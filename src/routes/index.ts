08c3c66609ebb1649039792d095c9f2c82414871
blvkroot <hsalukamba17@gmail.com>
2021-12-19 15:59:30 +0100

    default route information adjusted

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,30 @@
-import { Router } from 'express';
-import { encriptRouter } from './encripts/encript.routes';
+import { Router } from "express";
+import { encriptRouter } from "./encripts/encript.routes";
 
 const router = Router();
 
 // Home default route describing project
-router.get('/', (request, response) => {
-    response.json({
-        "version": "1.0.0",
-        "author": "Henriques Salucamba",
-        "project-name": "Facile-Challenge",
-        "description": "This project aims at encrypting and decrypting a given string"
-    });
+router.get("/", (request, response) => {
+  response.json({
+    version: "1.0.0",
+    author: "Henriques Salucamba",
+    "project-name": "Facile-Challenge",
+    description:
+      "This project aims at encrypting and decrypting a given string",
+    endpoints: [
+      {
+        GET: "/encripts/1",
+        POST: {
+          uri: "/encripts",
+          body: {
+            name: "Olá meu nome é string",
+          },
+        },
+      },
+    ],
+  });
 });
 
 router.use(encriptRouter);
 
-export { router }
+export { router };
